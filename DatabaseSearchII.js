26f30eb6c18dab69df773132c4176ae1b7db4724
Hiram Cruz <hiram0101@gmail.com>
2018-04-04 12:55:13 -0400

    added bonus II

---
+++ DatabaseSearchII.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const Database = {
+  store: {
+    heroes: [
+      { id: 1, name: 'Captain America', squad: 'Avengers' },
+      { id: 2, name: 'Iron Man', squad: 'Avengers' },
+      { id: 3, name: 'Spiderman', squad: 'Avengers' },
+      { id: 4, name: 'Superman', squad: 'Justice League' },
+      { id: 5, name: 'Wonder Woman', squad: 'Justice League' },
+      { id: 6, name: 'Aquaman', squad: 'Justice League' },
+      { id: 7, name: 'Hulk', squad: 'Avengers' },
+    ]
+  },
+  findOne: function(query) {
+    const result = this.store.heroes.find( obj => {
+      for(let key in query){
+        if(obj[key] !== query[key]) return false;
+      }
+      return true;
+    }) || null;
+    console.log(result);
+  },
+};
+
+Database.findOne({ id: 2 });
